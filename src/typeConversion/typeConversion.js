69fd28efa70ba191a797d11de85bdfe8ba400517
Your Name <bonoswanna@gmail.com>
2026-03-06 18:02:15 +0100

    Закончили с операторами

---
+++ src/typeConversion/typeConversion.js
@@ -0,0 +1,48 @@
+// не явные переобразование типов
+console.log("5" + 2)
+console.log("Hello " * "World")
+console.log(true)
+
+// явные переобразование
+let testStringNumber = "5"
+let testString = "Hello"
+let testBoolean = true
+let testEmptyString = ""
+let testUndefined = undefined
+let testNull = null
+console.log(
+    `Переобразование числа в строке в число ${Number(testStringNumber)}\n`, // 5
+    `Переобразование строки в число ${Number(testString)}\n`, // NaN
+    `Переобразование булиана в число ${Number(testBoolean)}\n`, // true = 1 если false = 0
+    `Переобразование пустой строки в число ${Number(testEmptyString)}\n`, //  0
+    `Переобразование undefined в число ${Number(testUndefined)}\n`, // NaN
+    `Переобразование null в число ${Number(testNull)}\n`, // 0
+)
+let testNumber = "5 105.5 test"
+console.log(parseInt(testNumber))
+console.log(parseFloat(testNumber))
+
+console.log(String(testNumber))
+console.log((testBoolean.toString()))
+console.log(Boolean(testString)) 
+// false 0 та -0, "" (порожній рядок), null, undefined, NaN, будут равны false
+const testObject = {
+    a:5
+}
+const testArray = []
+console.log(Boolean(testArray))
+
+console.log (0 == false)
+console.log(" \n" == 0)
+console.log(NaN === NaN)
+
+
+// Д/з
+let test = 1 / 0;
+test = String( test ); // "0"
+console.log(test)
+test = Boolean ( test ); // false
+console.log( test); // 0 
+console.log( true - 1 ); // 0
+console.log( 9 + false ); // 9
+console.log( null * 2 ); // 0
--- src/userInteraction/userInteraction.js
+++ src/userInteraction/userInteraction.js
@@ -3,15 +3,17 @@
 let age = prompt("Сколько вам лет?")
 // const  = confirm(`Вам ${age} лет?`)
 const prohibitionMessage = ()=>alert("Вам запрещено заходить на этот сайт")
-const adultAge = ()=> age < 18
-if (adultAge) {
+const adultAge = (checkingAge)=> checkingAge < 18
+console.log(age)
+if (adultAge(age)) {
     const isConfirmed = confirm(`Вам точно ${age}?`)
     if (isConfirmed) {
         prohibitionMessage()
     }
     else {
-        age = prompt("Введите корректный возраст", age) // тут ошибка
-        if (adultAge) {
+        age = prompt("Введите корректный возраст", age)
+        console.log(age) // тут ошибка
+        if (adultAge(age)) {
             prohibitionMessage()
         }
     }
