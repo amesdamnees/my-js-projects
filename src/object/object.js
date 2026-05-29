4a7e0656e6aaaa1ced54a4d376f75ae756245041
Your Name <bonoswanna@gmail.com>
2026-05-29 18:00:32 +0200

    Continue worked with objects

--- src/object/object.js
+++ src/object/object.js
@@ -30,20 +30,20 @@ let numberOfFruit = 6
 let myBag = {
     orange: 6,
     banana: 7,
-    [fruit]:numberOfFruit
+    [fruit]: numberOfFruit
 }
 console.log(myBag)
 // delete myBag[fruit]
 console.log(myBag)
-    let newBag = {}
-for (let key in myBag){
-    if(key === fruit){
+let newBag = {}
+for (let key in myBag) {
+    if (key === fruit) {
         continue
     }
-        newBag[key] = myBag[key]
+    newBag[key] = myBag[key]
 
 }
-myBag = {...newBag}
+myBag = { ...newBag }
 console.log(myBag)
 let myBagEntries = Object.entries(myBag)
 myBagEntries.forEach((item) => {
@@ -51,3 +51,48 @@ myBagEntries.forEach((item) => {
 })
 console.log(Object.keys(myBag))
 console.log(Object.values(myBag))
+console.log(myBag[fruit] && myBag[fruit].type, myBag.apple?.type)
+
+let myCar = {
+    name: "Toyota",
+    type: "Sedan",
+    productionYear: "2021",
+    colour: "Black",
+    adress: {
+        country: "Japan",
+        city: "Tokyo"
+    },
+    a: [
+        [
+            "b",
+            "c",
+            {
+                j: [
+                    0
+                ]
+            }
+        ]
+    ]
+
+}
+const myCarEntries = Object.entries(myCar)
+console.log(myCar["adress"]?.["street"])
+for (let i = 0; i < myCarEntries.length; i++) {
+    console.log(myCarEntries[i][0], ":", myCarEntries[i][1])
+}
+// console.log(myCarEntries)
+console.log(myCar.a[0][2].j[0])
+
+function conditionOfCar(car) {
+    if (!car) {
+        return "Вы ввели неверное авто"
+    }
+    const year = new Date().getFullYear()
+    let condition = "Это новое авто"
+    if (year - +car.productionYear >= 3) {
+        condition = "Это Б/У авто"
+    }
+
+    return condition
+}
+console.log(conditionOfCar(myCar))
